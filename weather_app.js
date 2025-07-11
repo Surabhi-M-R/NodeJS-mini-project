ee9df0d8e2febd2cf160a4c8880f058eb0a76b22
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-11 20:14:28 +0530

    final commit

--- weather_app.js
+++ weather_app.js
@@ -10,8 +10,26 @@ const rl=readline.createInterface({
 
 const getWeather= async (city)=>{
     const url=`${BASE_KEY}?q=${city}&appid=${API_KEy}&units=metric`;
+
+    try{
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(" City not found . Please check with the city name");
+        }
+        const weatherData=await response.json();
+        console.log("\n Weather information: ");
+        console.log(`City : ${weatherData.name}`);
+        console.log(`Temperature: ${weatherData.main.temp} C`);
+        console.log(`Description : ${weatherData.weather[0].description}`);
+        console.log(`Humidity: ${weatherData.main.humidity}%`);
+        console.log(`Wind speed : ${weatherData.wind.speed}m/s\n`);
+
+    }catch (error){
+        console.error(error);
+    }
 }
 
 const city=rl.question(" Enter your city to check weather");
 await getWeather(city);
 rl.close();
+
