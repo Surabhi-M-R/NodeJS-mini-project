32e3eede47c2d71e6de93cccf99e0f820f275642
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-11 19:50:19 +0530

    weather application initial commit

---
+++ weather_app.js
@@ -0,0 +1,10 @@
+import readline from 'readline/promises'
+
+const API_KEy ='';
+const BASE_KEY='';
+
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
