a2110e78f3f71eacd82a70b8bb6f9431b6df70b3
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-13 22:44:25 +0530

    third commit in creating server

--- URL/app.js
+++ URL/app.js
@@ -4,19 +4,29 @@ import { createServer } from 'http';
 import path from 'path';
 const PORT=3005;
 
-const server =createServer(async (req, res)=>{
-    if(req.method==="GET"){
-        if(req.url==="/"){  
-            try {
-                const data =await readFile(path.join("public","index.html"));
-                res.writeHead(200,{"Content-Type":"text/html"});
+const serveFile= async(res, filePath, contentType )=>{
+    try {
+                const data =await readFile(filePath);
+                res.writeHead(200,{"Content-Type":contentType});
                 res.end(data);
                 
             } catch (error) {
-                res.writeHead(404,{"Content-Type":"text/html"});
+                res.writeHead(404,{"Content-Type":contentType});
                 res.end(" 404 page not found ");
             }
+};
+
+const server =createServer(async (req, res)=>{
+    if(req.method==="GET"){
+        if(req.url==="/"){  
+            return serveFile(res, path.join("public","index.html"), "text/html")
+        }else if(req.method==='GET'){
+            if(req.url==='/style.css'){
+            return serveFile(res, path.join("public","style.css"),"text/css");
+            }
         }
+
+        
 
     }
 })
